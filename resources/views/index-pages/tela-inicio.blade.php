77a0d29f181658e5cbfa6f53cecc313e278f293e
Arthur Alves <art4321@outlook.com.br>
2020-09-28 18:49:34 -0300

    tela inicio

--- resources/views/index-pages/tela-inicio.blade.php
+++ resources/views/index-pages/tela-inicio.blade.php
@@ -5,76 +5,148 @@
         @component('componentes.header')
         @endcomponent
     <body>
-            <div class="tela-inicio container-fluid text-white" style="background: no-repeat center/cover url('{{ asset('img/img-telainicio.jpg')}}');">
-                <div class="item-fluid">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-12 text-center text-black my-5">
-                                <h1>Igreja Adventista do Sétimo Dia</h1>
-                                <div class="my-4 mx-5">
-                                    <div class="text-center text-black my-5">
-                                        <p>Lorem ipsum dolor sit amet,
-                                        consectetur adipiscing elit,
-                                        sed do eiusmod tempor incididunt
-                                        ut labore et dolore magna aliqua.
-                                        Ut enim ad minim veniam,
-                                        quis nostrud exercitation ullamco</p>
+                <div class="tela-inicio container-fluid text-white" style="background: no-repeat center/cover url('{{ asset('img/img-telainicio.jpg')}}');">
+                    <div class="container1 item-fluid">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-12 text-center text-black my-5">
+                                    <h1 class="titulo">IASD Central de Parnaíba</h1>
+                                    <div class="my-4 mx-5">
+                                        <div class="text-center text-black my-5">
+                                            <p>Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit,
+                                            sed do eiusmod tempor incididunt
+                                            ut labore et dolore magna aliqua.
+                                            Ut enim ad minim veniam,
+                                            quis nostrud exercitation ullamco</p>
+                                        </div>
                                     </div>
+                                    <button type="button" class="btmarcar-presenca" data-toggle="modal" data-target="#modalpresenca">Marcar Presença</button>
                                 </div>
-                                <button type="button" class="btmarcar-presenca" data-toggle="modal" data-target="#modalpresenca">Marcar Presença</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="container-fluid text-center text-white mais-informacoes">
-                <div class="container">
-                    <div class="row mb-15">
-                        <div class="col-md-12 mt-15 card-info">
-                            <div class="col-md-12">
-                                <h1>Mais Informações</h1>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 img-legenda-container mt-2 mb-2">
-                                    <div class="caption-imagem">
-                                        <img src="{{asset('img/novotempo.jpg')}}" class="img-fluid"/>
-                                        <div class="texto-caption">
-                                            <h2>Canal Novo Tempo</h2>
-                                            <a onclick="window.open('https://ntplay.com/aovivo')">
-                                                <button type="button" class="ntplay">Assistir ao vivo !</button>
-                                            </a>
+                    <div class="container-fluid text-center text-white sobreaigreja"style="background: no-repeat center/cover url('{{ asset('img/sobre.jpg')}}'); padding; 15px;">
+                        <div class="container2 container">
+                            <div class="row mb-15">
+                                <div class="row">
+                                    <div class="texto-caption">
+                                        <h2>Quem somos ?</h2>
+                                        <div class="legenda-imagem" style="margin-top: 15px;">
+                                            <h4>Quem são os adventistas ?</h4>
+                                            <p>Os adventistas do sétimo dia, com mais de 17 milhões de membros no mundo, são membros de uma igreja cristã protestante organizada em 1863 nos Estados Unidos.</p>
                                         </div>
-                                    </div>
-                                    <div class="legenda-imagem" style="margin-top: 15px;">
-                                        <h4>Legenda</h4>
-                                        <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor</p>
+                                        <div class="legenda-imagem" style="margin-top: 15px;">
+                                            <h4>Crenças</h4>
+                                            <p>Os adventistas consideram toda a Bíblia Sagrada como segura e única regra de fé e esperança. Suas doutrinas, portanto, seguem integralmente os ensinamentos bíblicos e nela estão fundamentadas. Estas crenças aqui expostas constituem a percepção e expressão que a Igreja sustém com respeito aos ensinos bíblicos.</p>
+                                        </div>
+                                        <div class="legenda-imagem" style="margin-top: 15px;">
+                                            <h4>Missão e Serviço</h4>
+                                            <p>A Igreja Adventista do Sétimo Dia crê na diversidade de dons e ministérios utilizados para cumprir com sua missão. Por essa razão, atua em diferentes áreas que realizam projetos, programas e ações estratégicas. Os ministérios, a partir da sede sul-americana adventista chamada Divisão Sul-Americana, são responsáveis pela produção de materiais, documentos oficiais e planejamentos gerais que beneficiam oito países.</p>
+                                        </div>
+                                        <a onclick="window.open('https://www.adventistas.org/pt/institucional/')">
+                                            <button type="button" class="sabermais">Saber Mais</button>
+                                        </a>
                                     </div>
                                 </div>
-                                <div class="col-md-6 img-legenda-container mt-2 mb-2">
-                                    <div class="caption-imagem">
-                                        <img src="{{asset('img/cursobiblico.jpg')}}" class="img-fluid"/>
+                            </div>
+                        </div>
+                    </div>
+
+                <div class="container-fluid text-center text-white mais-informacoes" style="padding: 20px">
+                        <div class="row mb-15">
+                            <div class="col-md-12 mt-15 card-info">
+                                <div class="col-md-12">
+                                    <h1>Quero Estudar !</h1>
+                                    <div style="background-color: rgb(20, 69, 155); width: 100%; height: 3px;"></div>
+                                </div>
+                                <div class="row linha1">
+                                    <div class="col-md-6 img-legenda-container mt-2 mb-2">
+                                        <div class="caption-imagem">
+                                            <img src="{{asset('img/cursobiblico.jpg')}}" class="img-fluid"/> <!-- Aqui ponto de css -->
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mt-5">
                                         <div class="texto-caption">
                                             <h2>Peça seu estudo bíblico</h2>
                                             <a onclick="window.open('https://biblia.com.br')">
                                                 <button type="button" class="crsbiblico">Pedir agora !</button>
                                             </a>
+                                            <div class="legenda-imagem" style="margin-top: 15px;">
+                                                <h4>Legenda</h4>
+                                                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor</p>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="legenda-imagem" style="margin-top: 15px;">
-                                        <h4>Legenda</h4>
-                                        <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor</p>
+                                </div>
+                                <div class="row linha2">
+                                    <div class="col-md-6 mt-5">
+                                        <div class="texto-caption">
+                                            <h2>Canal Novo Tempo</h2>
+                                            <a onclick="window.open('https://ntplay.com/aovivo')">
+                                                <button type="button" class="ntplay">Assistir ao vivo !</button>
+                                            </a>
+                                            <div class="legenda-imagem" style="margin-top: 15px;">
+                                                <h4>Legenda</h4>
+                                                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 img-legenda-container mt-2 mb-2">
+                                        <div class="caption-imagem">
+                                            <img src="{{asset('img/novotempo.jpg')}}" class="img-fluid"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row linha3">
+                                    <div class="col-md-6 img-legenda-container mt-2 mb-2">
+                                        <div class="caption-imagem">
+                                            <img src="{{asset('img/cursobiblico.jpg')}}" class="img-fluid"/> <!-- Aqui ponto de css -->
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mt-5">
+                                        <div class="texto-caption">
+                                            <h2>Peça seu estudo bíblico</h2>
+                                            <a onclick="window.open('https://biblia.com.br')">
+                                                <button type="button" class="crsbiblico">Pedir agora !</button>
+                                            </a>
+                                            <div class="legenda-imagem" style="margin-top: 15px;">
+                                                <h4>Legenda</h4>
+                                                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row linha4">
+                                    <div class="col-md-6 mt-5">
+                                        <div class="texto-caption">
+                                            <h2>Peça seu estudo bíblico</h2>
+                                            <a onclick="window.open('https://biblia.com.br')">
+                                            <button type="button" class="crsbiblico">Pedir agora !</button>
+                                        </a>
+                                        <div class="legenda-imagem" style="margin-top: 15px;">
+                                            <h4>Legenda</h4>
+                                            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 img-legenda-container mt-2 mb-2">
+                                    <div class="caption-imagem">
+                                        <img src="{{asset('img/cursobiblico.jpg')}}" class="img-fluid"/> <!-- Aqui ponto de css -->
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- <div class="row">
-                        <div class="col-md-12 d-flex justify-content-center">
-                            <a href="" class="btn btn-azul">Ver Todos +</a>
+                            <!-- </div> -->
                         </div>
-                    </div> -->
+                        <!-- <div class="row">
+                            <div class="col-md-12 d-flex justify-content-center">
+                                <a href="" class="btn btn-azul">Ver Todos +</a>
+                            </div>
+                        </div> -->
                 </div>
-            </div>
 
         <footer class="page-footer">
             <div class="footer-copyright text-center text-white py-3">
