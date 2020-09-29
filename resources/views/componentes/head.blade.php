fcbc7ffa8a53b0b5490e53a4751d7a282a69d6f9
Tiago-Alves-dos-Santos <tiagooliveiraaso@gmail.com>
2020-09-29 10:51:52 -0300

    /

--- resources/views/componentes/head.blade.php
+++ resources/views/componentes/head.blade.php
@@ -8,9 +8,9 @@
     <title>IASD - Central Parnaíba</title>
     <link rel="icon" href="{{asset('img/logo.jpg')}}" type=""/>
     {{-- bootstrap cdn  --}}
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" crossorigin="anonymous">
     {{-- css da pagina  --}}
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     {{-- Jquery 3.5.1 versao comprimida  --}}
     <script src="{{asset('js/jquery.min.js')}}"></script>
     {{-- conteudo dinamico de cabeçalho  --}}
