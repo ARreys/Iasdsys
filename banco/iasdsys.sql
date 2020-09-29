9440183ea287dc8b101b6ed7fa5c08f9495be7cd
Tiago-Alves-dos-Santos <tiagooliveiraaso@gmail.com>
2020-09-29 11:11:41 -0300

    banco add

---
+++ banco/iasdsys.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 29-Set-2020 às 16:05
+-- Versão do servidor: 10.4.14-MariaDB
+-- versão do PHP: 7.3.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `iasdsys`
+--
+CREATE DATABASE IF NOT EXISTS `iasdsys` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `iasdsys`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `evento`
+--
+
+CREATE TABLE `evento` (
+  `id` int(11) NOT NULL,
+  `criado` datetime DEFAULT NULL,
+  `alterado` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `titulo` varchar(45) DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `agendado_dia` date DEFAULT NULL,
+  `agendado_hora` varchar(45) DEFAULT NULL,
+  `foto` varchar(45) DEFAULT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `local_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Um evento é cadastrado por um usuario\nUm levento esta agendado em um local';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `local`
+--
+
+CREATE TABLE `local` (
+  `id` int(11) NOT NULL,
+  `criado` datetime DEFAULT NULL,
+  `alterado` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `capacidade` int(11) DEFAULT NULL,
+  `em_uso` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Um local pode conter varios eventos agendados\nUm local possui varias presenças de pessoas';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoa`
+--
+
+CREATE TABLE `pessoa` (
+  `id` int(11) NOT NULL,
+  `criado` datetime DEFAULT NULL,
+  `alterado` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `presenca`
+--
+
+CREATE TABLE `presenca` (
+  `id` int(11) NOT NULL,
+  `criado` datetime DEFAULT NULL,
+  `alterado` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `quantidade_marcada` int(11) DEFAULT NULL,
+  `pessoa_id` int(11) NOT NULL,
+  `local_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `criado` datetime DEFAULT NULL,
+  `alterado` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `senha` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Um usuario cadastrar muito eventos';
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id`,`usuario_id`,`local_id`),
+  ADD KEY `fk_evento_usuario1_idx` (`usuario_id`),
+  ADD KEY `fk_evento_local1_idx` (`local_id`);
+
+--
+-- Índices para tabela `local`
+--
+ALTER TABLE `local`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `pessoa`
+--
+ALTER TABLE `pessoa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `presenca`
+--
+ALTER TABLE `presenca`
+  ADD PRIMARY KEY (`id`,`pessoa_id`,`local_id`),
+  ADD KEY `fk_presenca_pessoa_idx` (`pessoa_id`),
+  ADD KEY `fk_presenca_local1_idx` (`local_id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `local`
+--
+ALTER TABLE `local`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pessoa`
+--
+ALTER TABLE `pessoa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `presenca`
+--
+ALTER TABLE `presenca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `evento`
+--
+ALTER TABLE `evento`
+  ADD CONSTRAINT `fk_evento_local1` FOREIGN KEY (`local_id`) REFERENCES `local` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_evento_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `presenca`
+--
+ALTER TABLE `presenca`
+  ADD CONSTRAINT `fk_presenca_local1` FOREIGN KEY (`local_id`) REFERENCES `local` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_presenca_pessoa` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
