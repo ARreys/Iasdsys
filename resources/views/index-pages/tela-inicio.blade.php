f63ce3ec2ec3759689888b8329feccbca2213b04
Arthur Alves <art4321@outlook.com.br>
2020-10-04 19:36:43 -0300

    correção titulo

--- resources/views/index-pages/tela-inicio.blade.php
+++ resources/views/index-pages/tela-inicio.blade.php
@@ -76,7 +76,7 @@
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="{{asset('img/cursobiblico.jpg')}}" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
                         <div class="card-body">
-                            <h2>Peça seu estudo bíblico</h2>
+                            <h2>Estudo bíblico</h2>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <a onclick="window.open('https://biblia.com.br')">
